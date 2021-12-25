a156c3b811f0d13817d4f5e147d28f4ce9d4a1cc
islamshai <shaiful.islam@leaflabs.de>
2021-12-25 15:23:17 +0100

    removing the extra function from api_services.domain.data_fetching.py file, combined those 2 functions in one.

--- api_services/domain/data_fetching.py
+++ api_services/domain/data_fetching.py
@@ -1,19 +1,10 @@
 import logging
 
 from django.core.exceptions import ObjectDoesNotExist
-from django.forms import model_to_dict
 
 from api_services.models import FileDetail
 from file_upload.models import Document
 from django.utils.translation import ugettext as _
-
-
-def fetch_all_entries_from_latest_file():
-    """
-    getting all the file details using reverse Django manager
-    :return:
-    """
-    return Document.objects.latest('created').file_detail.all()
 
 
 def get_random_line_from_latest_file():
@@ -22,10 +13,10 @@ def get_random_line_from_latest_file():
     added file, given that the file is a txt file.
     :return:
     """
-    file_details = fetch_all_entries_from_latest_file()
-    if not file_details:
+    try:
+        return Document.objects.latest('created').file_detail.order_by('?').first().to_dict()
+    except ObjectDoesNotExist:
         return _('Last file that you uploaded could not be parsed')
-    return file_details.order_by('?').first().to_dict()
 
 
 def get_random_line_backward(line_dict) -> str:
@@ -37,7 +28,7 @@ def get_random_line_backward(line_dict) -> str:
 
 def get_x_longest_lines(number_of_lines: int) -> dict:
     try:
-        largest_lines = FileDetail.objects.all().values('line_content')[:number_of_lines]
+        largest_lines = FileDetail.objects.values('line_content')[:number_of_lines]
         if largest_lines:
             return {str(index): line.get('line_content') for index, line in enumerate(largest_lines)}
         return {}
