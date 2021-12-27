92a31a2b4b43c4355c92f0e7c1d14c20bfe1c4cd
islamshai <shaiful.islam@leaflabs.de>
2021-12-27 19:21:13 +0100

    adding import export package to this web service and drf-spectacular package for having api_documentation and enabling data import and export.

--- api_services/urls.py
+++ api_services/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 from . import views
 
@@ -10,3 +11,11 @@ urlpatterns = [
     path('hundreds-longest-lines/', views.hundreds_longest_line, name='hundreds_longest_lines'),
     path('twenty-longest-lines/', views.twenty_longest_line_of_last_file, name='twenty_longest_lines'),
 ]
+
+drf_spectacular_urls = [
+    path('api/schema/', SpectacularAPIView.as_view(urlconf=urlpatterns), name='api_schema'),
+    path('api/docs/swagger-ui/', SpectacularSwaggerView.as_view(url_name='api_services:api_schema'), name='api_sa'),
+    path('api/docs/', SpectacularRedocView.as_view(url_name='api_services:api_schema'), name='api_'),
+]
+
+urlpatterns += drf_spectacular_urls
